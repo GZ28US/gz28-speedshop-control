dbf577235f3bde1227fcb3d9e6024ce3facb4954
Marcio De Maria <gz28us@gmail.com>
2026-05-17 01:40:47 -0400

    Added shared Header component

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+export default function Header() {
+  return (
+    <div className="mb-10">
+      <h1 className="text-5xl font-bold mb-8">
+        GZ28US Financial CONTROL
+      </h1>
+
+      <div className="flex gap-4">
+        <a
+          href="/clients"
+          className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold"
+        >
+          CLIENTS
+        </a>
+
+        <button className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold">
+          RIDES
+        </button>
+
+        <button className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold">
+          STAFF
+        </button>
+      </div>
+    </div>
+  )
+}
