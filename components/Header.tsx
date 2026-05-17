c9971f7f6fe85456ce8dfcb55eb3f5b749f8c6ba
Marcio De Maria <gz28us@gmail.com>
2026-05-17 01:43:14 -0400

    Added HOME button to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,14 @@ export default function Header() {
         GZ28US Financial CONTROL
       </h1>
 
-      <div className="flex gap-4">
+      <div className="flex gap-4 flex-wrap">
+        <a
+          href="/"
+          className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold"
+        >
+          HOME
+        </a>
+
         <a
           href="/clients"
           className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold"
