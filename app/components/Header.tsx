5c40d9441ecf5ec30ab8c60f46f46a53fefb602a
Marcio De Maria <gz28us@gmail.com>
2026-05-17 21:03:48 -0400

    Header UpDate

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,21 +7,34 @@ export default function Header() {
         GZ28US Financial CONTROL
       </h1>
 
-      <div className="flex gap-4">
-        <a
+      <div className="flex gap-4 flex-wrap">
+        
+          href="/"
+          className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold"
+        >
+          HOME
+        </a>
+
+        
           href="/clients"
           className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold"
         >
           CLIENTS
         </a>
 
-        <button className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold">
+        
+          href="/rides"
+          className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold"
+        >
           RIDES
-        </button>
+        </a>
 
-        <button className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold">
+        
+          href="/staff"
+          className="bg-gray-900 hover:bg-gray-700 border border-gray-700 px-6 py-4 rounded-2xl text-xl font-bold"
+        >
           STAFF
-        </button>
+        </a>
       </div>
     </div>
   )
